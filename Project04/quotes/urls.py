d199a300adf3a0230ec11db5a2c2504a0405e833
Anisa Ahamed <anisa@Sams-MacBook-Pro-2.local>
2020-11-18 22:12:42 -0700

    created base.html

--- Project04/quotes/urls.py
+++ Project04/quotes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home.html', view.home, name="home")
+    path('', views.home, name="home"),
+    
+    path('about.html', views.about, name="about"),
+
 ]
