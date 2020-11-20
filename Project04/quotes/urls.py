8bcf116708a175ba4f7f21318646d0a740119340
Anisa Ahamed <anisa@Sams-MacBook-Pro-2.local>
2020-11-20 11:54:48 -0700

    added delete function

--- Project04/quotes/urls.py
+++ Project04/quotes/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
 
     path('add_stock.html', views.add_stock, name="add_stock"),
 
+    path('delete/<stock_id>', views.delete, name="delete"),
+
+
 ]
--- Project04/quotes/views.py
+++ Project04/quotes/views.py
@@ -35,12 +35,18 @@ def add_stock(request):
 
         if form.is_valid():
             form.save()
-            messages.success(request, ("Stock Has Been Added Successsfully"))
+            messages.success(request, ("Stock Has Been Added Successsfully!"))
             return redirect ('add_stock')
     else:
 
         ticker = Stock.objects.all()
         return render(request, 'add_stock.html', {'ticker': ticker})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock Has Been Deleted!"))
+    return redirect ('add_stock')
 
 
 
