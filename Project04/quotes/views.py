a3e66e49379134b28946d1f4a028a5115888474d
Anisa Ahamed <anisa@Sams-MacBook-Pro-2.local>
2020-11-19 21:14:45 -0700

    created a view

--- Project04/quotes/views.py
+++ Project04/quotes/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    #pk_378fb4b735894bae8434380e31b2f915
+    
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_378fb4b735894bae8434380e31b2f915")
+
+    try:
+        api = json.loads(api_request.content)
+    except Expection as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
